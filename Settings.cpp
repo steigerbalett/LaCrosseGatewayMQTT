f951de483750d72bbd82aa6a1b9e2f129cbcfeb3
steigerbalett <40899113+steigerbalett@users.noreply.github.com>
2026-04-04 09:18:19 +0200

    update settings.cpp

--- Settings.cpp
+++ Settings.cpp
@@ -163,13 +163,6 @@ String Settings::ToString() {
 bool Settings::FromString(String settings) {
   bool result = false;
 
-  if (key.length() > 0 && value.length() > 0) {
-    Add(key, value);
-    result = true;   // ← hinzufügen
-    key = "";
-    value = "";
-  }
-
   settings.trim();
   if (!settings.endsWith(";")) {
     settings += ";";
@@ -193,6 +186,7 @@ bool Settings::FromString(String settings) {
         value.trim();
         if (key.length() > 0 && value.length() > 0) {
           Add(key, value);
+          result = true;
           key = "";
           value = "";
         }
