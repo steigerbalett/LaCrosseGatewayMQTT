6b5471ffb8b5358981bf176d59ef8e6a233dcdc4
steigerbalett <40899113+steigerbalett@users.noreply.github.com>
2026-04-14 17:05:10 +0200

    Release 2026.04.14

--- LaCrosseGateway.ino
+++ LaCrosseGateway.ino
@@ -1,5 +1,5 @@
 #define PROGNAME         "LaCrosseITPlusReader.Gateway"
-#define PROGVERS         "1.36.49"
+#define PROGVERS         "1.36.50"
 
 #define RFM1_SS          15
 #define RFM2_SS          2
