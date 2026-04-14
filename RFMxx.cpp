3a5c360744ca5b8f092be98ead2e6ad3d44b3316
steigerbalett <40899113+steigerbalett@users.noreply.github.com>
2026-04-14 16:43:43 +0200

    radio again

--- RFMxx.cpp
+++ RFMxx.cpp
@@ -132,10 +132,6 @@ RFMxx::RFMxx(byte mosi, byte miso, byte sck, byte ss, byte irq, TPinCallback pin
 }
 
 bool RFMxx::Begin(bool isPrimary) {
-  // GPIO-Pins initialisieren (hier statt im Konstruktor, da Begin() nach Arduino init() läuft)
-  pinMode(m_mosi, OUTPUT);
-  pinMode(m_miso, INPUT);
-  pinMode(m_sck, OUTPUT);
   if(!m_pinCallback) {
     pinMode(m_ss, OUTPUT);
     digitalWrite(m_ss, HIGH);
